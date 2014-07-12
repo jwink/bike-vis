d0062e4394b0d3f2ba77e6c9c3af57656ec2fed0
jwink <jeff_winkler@yahoo.com>
2014-07-12 15:23:57 -0400

    some initial layering

--- app/assets/javascripts/bike_geo.js
+++ app/assets/javascripts/bike_geo.js
@@ -1,7 +1,18 @@
 $(function(){
-function geo(){
-  console.log('geo is here')
-}
+
+favIcon = L.divIcon({
+  className: 'fav-icon',
+  iconSize: [10,10],
+  iconAnchor: [2,2],
+  popupAnchor: [-8,0]
+});
+
+nonFavIcon = L.divIcon({
+  className: 'non-fav-icon',
+  iconSize: [10,10],
+  iconAnchor: [2,2],
+  popupAnchor: [-8,0]
+});
 
     var popup = L.popup();
 
@@ -13,59 +24,78 @@ function geo(){
     }
 
     //map1.on('click', onMapClick);
-
-
 });
 
 fromStation = undefined;
 fromStationNear = undefined;
-  toStation = undefined;
-  toStationNear = undefined;
+toStation = undefined;
+toStationNear = undefined;
+
+favArray = [];
+nonFavArray = [];
 
 function populateMap(data) {
-  console.log(data[0]);
+  user = $('.user-id-span').data('user');
   $.each(data, function(index, station) {
     var latitude = station.latitude;
     var longitude = station.longitude;
     var label = station.label;
     var bikes = station.availableBikes;
     var docks = station.availableDocks;
-    var $el = label + " <button data-bike=" + station.id + " class='pick-up-button'>Pick Up</button>" + " <button data-bike=" + station.id + " class='drop-off-button'>Drop Off</button>"
-    var greenIcon = L.icon({
-      iconUrl: 'http://api.tiles.mapbox.com/mapbox.js/v1.6.3/images/marker-icon-2x.png',
-      //shadowUrl: 'http://api.tiles.mapbox.com/mapbox.js/v1.6.3/images/marker-shadow.png',
-      iconSize:     [10, 10], // size of the icon
-      //shadowSize:   [5, 5], // size of the shadow
-      iconAnchor:   [2, 2], // point of the icon which will correspond to marker's location
-      //shadowAnchor: [4, 4],  // the same for the shadow
-      popupAnchor:  [-8, 0] // point from which the popup should open relative to the iconAnchor
-    });
-    marker = L.marker([latitude, longitude], {icon: greenIcon})
-            .addTo(map1)
-            .bindPopup($el);
+    var $el = "<strong>"+label+"</strong>" + " &nbsp&nbsp&nbsp&nbspBikes: " + bikes + " Docks: " + docks +
+              " </br><button data-bike=" + station.id +
+              " class='pick-up-button'>Pick Up</button>" +
+              " <button data-bike=" + station.id +
+              " class='drop-off-button'>Drop Off</button>";
+
+
+
+    //$el += " <button>Yo</button>"
+
+    if (user=='no_user') {
+      var marker = L.marker([latitude, longitude], {icon: nonFavIcon})
+              .bindPopup($el);
+      nonFavArray.push(marker);
+    } else {
+      //  test if station id in favorites, if so add to favarray,
+      //  else to non fav
+      //  also add either add or remove fav button depending
+    }
   });
+
+  nonFavStations = L.featureGroup(nonFavArray);
+
+  if (user=='no_user') {
+    var overlayMaps = {
+      "All Stations": nonFavStations
+    }
+  } else {
+    var overlayMaps = {
+
+    }
+  }
+
+  map1.addLayer(nonFavStations);
+
+  L.control.layers(null, overlayMaps).addTo(map1);
 
   map1.on('popupopen', function() {
-
-
-
-  $('.pick-up-button').click(function() {
-      console.log($('.pick-up-button').data('bike'), "pickup");
-      wait = true;
-      selectedStation = $('.pick-up-button').data('bike');
-      populateStationInfo(selectedStation, "from");
-      waiting = setInterval(function(){
-        if (currStationNearby.models[4].history.length==24 && wait==true) {
-          wait = false;
-          findBestAlternative(currStation, currStationNearby);
-          fromStation = currStation;
-          fromStationNear = currStationNearby;
-        } else {
-          console.log("waiting");
-        }
-      }, 1000);
-
-  });
+    $('.pick-up-button').click(function() {
+        console.log($('.pick-up-button').data('bike'), "pickup");
+        wait = true;
+        selectedStation = $('.pick-up-button').data('bike');
+        populateStationInfo(selectedStation, "from");
+        waiting = setInterval(function(){
+          if (currStationNearby.models[4].history.length==24 && wait==true) {
+            wait = false;
+            findBestAlternative(currStation, currStationNearby);
+            fromStation = currStation;
+            fromStationNear = currStationNearby;
+          } else {
+            console.log("waiting");
+          }
+        }, 1000);
+    });
 
     $('.drop-off-button').click(function() {
       console.log($('.drop-off-button').data('bike'), "dropoff");
@@ -82,9 +112,6 @@ function populateMap(data) {
           console.log("waiting");
         }
       }, 1000);
-
     });
-
-});
-
+  });
 }
