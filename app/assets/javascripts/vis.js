0a6325a0f5322e20ae8935aa989d1dcaae9ed93d
jwink <jeff_winkler@yahoo.com>
2014-07-14 13:06:59 -0400

    what

--- app/assets/javascripts/vis.js
+++ app/assets/javascripts/vis.js
@@ -3,7 +3,90 @@ $(function() {
 
   var inVis = $('.in-vis-span').data('vis');
   if (inVis == "in-vis") {
-    // function for map
+    getSaturations();
   }
 
 });
+
+counter = 0;
+
+function getSaturations() {
+  $.ajax({
+    url: '/saturations/vis',
+    dataType: 'json',
+    success: function(data) {
+      globalSaturations = data;
+      timeOutID = setInterval(runVis, 1000);
+      //runVis();
+
+    }
+  });
+}
+
+
+function runVis() {
+  if (counter > 0) {
+    map1.removeLayer(stationGroup);
+  }
+  $('#hour').html(counter);
+  stationInfo = thisHour(counter);
+  counter = counter + 1;
+  if (counter == 24) {
+    clearInterval(timeOutID);
+  }
+  layerInfo = []
+  $.each(stationInfo, function(index, station) {
+  var marker = L.circleMarker([station.latitude, station.longitude], {radius: (station.saturation * 20),
+                         color: '#000000',
+                         fillColor: getFillColor(station.quadrant),
+                         opacity: 1,
+                         stroke: true,
+                         fillOpacity: getOpacity(station.quadsatur)});
+    layerInfo.push(marker);
+  });
+  stationGroup = L.featureGroup(layerInfo);
+  map1.addLayer(stationGroup);
+}
+
+function thisHour(hour) {
+  hourArray = []
+  $.each(globalSaturations, function(index, timeStation) {
+    if (timeStation.hour == hour) {
+      hourArray.push(timeStation);
+    }
+  });
+  console.log(hourArray);
+  return hourArray;
+}
+
+
+function getOpacity(quadSaturation) {
+  return (quadSaturation - .05) * 1.4;
+
+}
+
+function getFillColor(quad) {
+  switch(quad) {
+    case "se":
+      return '#0000ff';
+      break;
+    case "ne":
+      return '#00ffff';
+      break;
+    case "nw":
+      return '#00ff00'
+      break;
+    case "sw":
+      return '#ff00ff';
+      break;
+    default:
+      return '#ffff00';
+      break;
+
+  }
+
+}
+
+
+
+
