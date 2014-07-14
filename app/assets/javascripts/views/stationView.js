683ca9e2bc99fd81d9898c181b53094ee1d20b82
paulh1am <pshiam@gmail.com>
2014-07-14 08:23:53 -0400

    StationView in progress for adding Trip to sidebar

--- app/assets/javascripts/views/stationView.js
+++ app/assets/javascripts/views/stationView.js
@@ -4,6 +4,21 @@ function StationView(station) {
   this.el = undefined;
 }
 
+//GO GET THIS.MODEL. ALL THE DATA TO SHOW - SHOW IT IN THE RIGHT PLACE FROM/TO
 StationView.prototype.render = function() {
+  
+    newDiv = $("<div class='selected'>");
+    newUl = $("<ul class='station-info'>");
+    newDiv.append(newUl);  
+
+    function addLi(li){
+      newUl.append("<li class='station-detail'>").html(li);
+    }
+
+    addLi(this.model.current.label);
+    addLi(this.model.current.availableBikes);
+
+
+    this.el = newDiv;
 
 }
