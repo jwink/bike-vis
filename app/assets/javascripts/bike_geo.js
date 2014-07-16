37ff8b4c819e0f9e83c8ab18897ba3c62a0165a8
jwink <jeff_winkler@yahoo.com>
2014-07-16 08:45:56 -0400

    what

--- app/assets/javascripts/bike_geo.js
+++ app/assets/javascripts/bike_geo.js
@@ -1,19 +1,24 @@
-$(function(){
 
+fromStation = undefined;
+fromStationNear = undefined;
+toStation = undefined;
+toStationNear = undefined;
+favArray = [];
+nonFavArray = [];
+
+$(function(){
   prevFromFillColor = undefined;
   prevFromFillOpacity = undefined;
   prevFromSource = undefined;
   prevToFillColor = undefined;
   prevToFillOpacity = undefined;
   prevToSource = undefined;
-
   prevFromAltFillColor = undefined;
   prevFromAltFillOpacity = undefined;
   prevFromAltMarker = undefined;
   prevToAltFillColor = undefined;
   prevToAltFillOpacity = undefined;
   prevToAltMarker = undefined;
-
   map1.on('zoomend', function() {
     var inVis = $('.in-vis-span').data('vis');
     if (inVis != "in-vis") {
@@ -29,17 +34,11 @@ $(function(){
       }
     }
   });
-
-
-
   map1.on('popupopen', function(e) {
-    //var popupObject = e;
     $('.pick-up-button').click(function() {
       if (prevFromSource != undefined) {
         prevFromSource.setStyle({fillOpacity: prevFromFillOpacity, fillColor: prevFromFillColor});
       }
-      //console.log(e);
-      //console.log($('.pick-up-button').data('bike'), "pickup");
       wait = true;
       selectedStation = $('.pick-up-button').data('bike');
       populateStationInfo(selectedStation, "from");
@@ -50,10 +49,8 @@ $(function(){
           fromStation = currStation;
           fromStationNear = currStationNearby;
           populateFromEl();
-          //console.log(fromStation.bestAlternative.current.id);
           chageAltColor(fromStation.bestAlternative.current.id, "from");
         } else {
-          //console.log("waiting");
         }
       }, 1000);
       map1.closePopup();
@@ -61,14 +58,11 @@ $(function(){
       prevFromFillOpacity = e.popup._source.options.fillOpacity;
       prevFromFillColor = e.popup._source.options.fillColor;
       e.popup._source.setStyle({fillOpacity: 0.85, fillColor: '#00ff00'});
-
     });
-
     $('.drop-off-button').click(function() {
       if (prevToSource != undefined) {
         prevToSource.setStyle({fillOpacity: prevToFillOpacity, fillColor: prevToFillColor});
       }
-      //console.log($('.drop-off-button').data('bike'), "dropoff");
       wait = true;
       selectedStation = $('.pick-up-button').data('bike');
       populateStationInfo(selectedStation, "to");
@@ -81,7 +75,6 @@ $(function(){
           populateToEl();
           chageAltColor(toStation.bestAlternative.current.id, "to");
         } else {
-          console.log("waiting");
         }
       }, 1000);
       map1.closePopup();
@@ -90,17 +83,14 @@ $(function(){
       prevToFillColor = e.popup._source.options.fillColor;
       e.popup._source.setStyle({fillOpacity: 0.85, fillColor: '#00ff00'});
     });
-
    $('.add-as-fav').click(function() {
      whichStation = $('.add-as-fav').data('bike');
      whichUser = $('.user-id-span').data('user');
      favorites.create({user_id: whichUser, station_id: whichStation});
      window.location.reload();
    });
-
    $('.drop-as-fav').click(function() {
      var thisFav = {};
-     //  add logic to delete a fav on server side
      var whichStation = $('.drop-as-fav').data('bike');
      $.each(favorites.models, function(index, fav) {
        if (fav.attributes.citibike_id == whichStation) {
@@ -110,47 +100,11 @@ $(function(){
      thisFav.destroy();
      window.location.reload();
    });
-
-
   });
-
-
-  favIcon = L.divIcon({
-    className: 'fav-icon',
-    iconSize: [10,10],
-    iconAnchor: [2,2],
-    popupAnchor: [-8,0]
-  });
-
-  nonFavIcon = L.divIcon({
-    className: 'non-fav-icon',
-    iconSize: [10,10],
-    iconAnchor: [2,2],
-    popupAnchor: [-8,0]
-  });
-
-    // var popup = L.popup();
-
-    // function onMapClick(e) {
-    //   popup
-    //     .setLatLng(e.latlng)
-    //     .setContent("You clicked the map at " + e.latlng.toString())
-    //     .openOn(map1);
-    // }
-
-    // //map1.on('click', onMapClick);
 });
 
-fromStation = undefined;
-fromStationNear = undefined;
-toStation = undefined;
-toStationNear = undefined;
-
-favArray = [];
-nonFavArray = [];
 
 function populateMap(data) {
-
   var user = $('.user-id-span').data('user');
   var userFavorites = [];
   if (user != "no_user") {
@@ -158,7 +112,6 @@ function populateMap(data) {
       userFavorites.push(fav.attributes.citibike_id);
     });
   }
-
   $.each(data, function(index, station) {
     var latitude = station.latitude;
     var longitude = station.longitude;
@@ -166,14 +119,11 @@ function populateMap(data) {
     var bikes = station.availableBikes;
     var docks = station.availableDocks;
     var saturation = (bikes/(bikes + docks));
-    //console.log(saturation);
     var $el = "<strong>"+label+"</strong>" + " &nbsp&nbsp&nbsp&nbspBikes: " + bikes + " Docks: " + docks +
               " </br><button data-bike=" + station.id +
               " class='pick-up-button'>Pick Up</button>" +
               " <button data-bike=" + station.id +
               " class='drop-off-button'>Drop Off</button>";
-
-
     if (user=='no_user') {
       var marker = L.circleMarker([latitude, longitude], {radius: 8,
                          color: '#000000',
@@ -183,10 +133,6 @@ function populateMap(data) {
                          className: station.id.toString(),
                          fillOpacity: saturation})
                      .bindPopup($el, {offset: [-12, 2]});
-
-
-      //var marker = L.marker([latitude, longitude], {icon: nonFavIcon})
-      //        .bindPopup($el);
       nonFavArray.push(marker);
     } else {
       if ($.inArray(station.id, userFavorites) != -1) {
@@ -200,10 +146,6 @@ function populateMap(data) {
                          className: station.id.toString(),
                          fillOpacity: (saturation+0.2)})
                      .bindPopup($el, {offset: [-12, 2]});
-
-
-        //var marker = L.marker([latitude, longitude], {icon: favIcon})
-        //      .bindPopup($el);
         favArray.push(marker);
       } else {
         $el += " <button data-bike=" + station.id +
@@ -216,23 +158,11 @@ function populateMap(data) {
                          className: station.id.toString(),
                          fillOpacity: saturation})
                      .bindPopup($el, {offset: [-12, 2]});
-
-        //var marker = L.marker([latitude, longitude], {icon: nonFavIcon})
-        //      .bindPopup($el);
         nonFavArray.push(marker);
       }
     }
   });
-
-  //  in use marker...create two markers, one for max one for now
-  //  add into nonFavArray
-
-
-
-
   nonFavStations = L.featureGroup(nonFavArray);
-
-
   if (user=='no_user') {
     var overlayMaps = {
       "All Stations": nonFavStations
@@ -246,14 +176,10 @@ function populateMap(data) {
     map1.addLayer(favStations);
     L.control.layers(null, overlayMaps).addTo(map1);
   }
-
   map1.addLayer(nonFavStations);
 }
 
-
-
 function chageAltColor(id, direction) {
-  //console.log(id);
   map1.eachLayer(function(marker) {
     if (marker.options != undefined) {
       if (marker.options.stroke != undefined) {
@@ -263,13 +189,10 @@ function chageAltColor(id, direction) {
       }
     }
   });
-
-
   if (direction == "from") {
     if (prevFromAltMarker != undefined) {
       prevFromAltMarker.setStyle({fillColor: prevFromAltFillColor, fillOpacity: prevFromAltFillOpacity});
     }
-    // populate prev vars, update curr colors
     prevFromAltFillColor = currAlt.options.fillColor;
     prevFromAltFillOpacity = currAlt.options.fillOpacity;
     prevFromAltMarker = currAlt;
@@ -278,13 +201,11 @@ function chageAltColor(id, direction) {
     if (prevToAltMarker != undefined) {
       prevToAltMarker.setStyle({fillColor: prevToAltFillColor, fillOpacity: prevToAltFillOpacity});
     }
-    // populate prev vars, update curr colors
     prevToAltFillColor = currAlt.options.fillColor;
     prevToAltFillOpacity = currAlt.options.fillOpacity;
     prevToAltMarker = currAlt;
     currAlt.setStyle({fillColor: '#ffff00', fillOpacity: 0.85});
   }
-
 }
 
 
--- app/assets/javascripts/bike_vis_app.js
+++ app/assets/javascripts/bike_vis_app.js
@@ -8,46 +8,20 @@ window.BikeVisApp = {
 };
 
 $(document).ready(function(){
-
   favorites = new BikeVisApp.Collections.Favorites();
-  //listView = new BikeVisApp.Views.FavoritesListView({collection: favorites, el: $('#favorites')});
   favorites.fetch();
-
   map1 = L.mapbox.map('map1', 'jeffwinkler.iolcgn8o')
     .setView([40.725, -73.979], 13);
-
-
-  //map1 = L.mapbox.map('map1', 'jeffwinkler.ipa0jko0  ')
-  //  .setView([40.725, -73.979], 13);
-
-
   globalCurrentData = undefined;
-  //BikeVisApp.initialize();
   var inVis = $('.in-vis-span').data('vis');
   if (inVis != "in-vis") {
     if (inVis!="static") {
       currentData();
     }
   }
-
-
-  // $('#station-select').on('change', function() {
-  //   wait = true;
-  //   selectedStation = $('#station-select').val();
-  //   populateStationInfo(selectedStation, "to");
-  //   waiting = setInterval(function(){
-  //     if (currStationNearby.models[4].history.length==24 && wait==true) {
-  //       wait = false;
-  //       findBestAlternative(currStation, currStationNearby);
-  //     } else {
-  //       console.log("waiting");
-  //     }
-  //   }, 1000);
-  // });
 });
 
 function populateStationInfo(whichStation, direction) {
-
   $.each(globalCurrentData, function(index, station) {
    if (station.id == whichStation) {
       currInfoObject = station;
@@ -55,7 +29,6 @@ function populateStationInfo(whichStation, direction) {
   });
   currStation = new StationModel(currInfoObject, direction);
   currStation.getHistory.call(currStation)();
-
   currStationNearby = new StationCollection();
   var stationsArr = []
   $.each(currStation.current.nearbyStations, function(index, nearby) {
@@ -67,7 +40,6 @@ function populateStationInfo(whichStation, direction) {
     });
     nearStation = new StationModel(currInfoObject, direction);
     nearStation.getHistory.call(nearStation)();
-
     currStationNearby.models.push(nearStation);
   });
 }
